ebebae3a45c054af94df15fe9e17caabd09a9fff
kritbovorn <kritbovorn@gmail.com>
2022-12-18 13:16:07 +0700

    --- add : enum()

---
+++ src/enum/enum_api.ts
@@ -0,0 +1,8 @@
+export enum ApiUrl {
+    posts = "https://jsonplaceholder.typicode.com/posts",
+    users = "https://jsonplaceholder.typicode.com/users",
+    todos = "https://jsonplaceholder.typicode.com/todos",
+    comments = "https://jsonplaceholder.typicode.com/comments",
+    albums = "https://jsonplaceholder.typicode.com/albums",
+    photos = "https://jsonplaceholder.typicode.com/photos"
+}
